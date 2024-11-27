2e1f33332a0638ed1f6cae9b28cb80177aac236a
Kaustubh <132165310+kaustubh-aras@users.noreply.github.com>
2024-11-27 13:00:18 +0530

    chore: add Constants.js file for managing global constants

---
+++ src/app/constants.js
@@ -0,0 +1,13 @@
+export const DevelopmentPlanData = {
+  title: "Development Plan",
+  description:
+    "The Development Plan Section outlines the strategic blueprint for transforming the vision of the project into reality. It encompasses a detailed roadmap that highlights the key stages of development, including planning, design, construction, and operational rollout.",
+};
+
+export const AboutCamayaData = {
+  title: "About Camaya Coast",
+  subDescription:
+    "Nestled in the heart of Bataan, Camaya Coast offers a perfect blend of nature and modernity. Enjoy the beauty of pristine beaches, world-class amenities, and a vibrant community.",
+  description:
+    "Brief description about Camaya Coast Properties and its vision to provide luxurious beachfront living",
+};
