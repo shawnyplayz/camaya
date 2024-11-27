ec00d86817a2dade5718bcab939ab0af27aee01c
Kaustubh <132165310+kaustubh-aras@users.noreply.github.com>
2024-11-27 14:42:53 +0530

    feat: Create `ContactForm` component with fields for name, email, phone, and message.

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Input from "./Input";
+import Button from "./Button";
+
+const ContactForm = () => {
+  return (
+    <div className="bg-white px-10 py-8 rounded-lg shadow-md max-w-md mx-auto lg:max-w-xl">
+      <form>
+        <h1 className="text-[#221C42] font-workSansMedium font-medium text-3xl sm:text-4xl md:text-5xl mb-6 mt-5">
+          Contact Us
+        </h1>
+        <div className="w-full">
+          <Input type="text" placeholder="Name" />
+          <Input type="email" placeholder="Email" />
+          <Input type="text" placeholder="Phone" />
+          <textarea
+            id="message"
+            name="message"
+            rows="5"
+            placeholder="Message"
+            className="w-full pl-7 py-6 rounded-xl border-2 mb-8 resize-none focus:outline-none focus:ring-2 focus:ring-[#221C42]"
+          />
+        </div>
+        <div className="flex items-center justify-center pb-8">
+          <Button>Send Message</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
