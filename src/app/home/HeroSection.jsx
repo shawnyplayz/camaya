71ab9c824f2af35124b4adda70bab3048f1fc027
Kaustubh <132165310+kaustubh-aras@users.noreply.github.com>
2024-11-27 13:00:44 +0530

    feat: add Hero section component

---
+++ src/app/home/HeroSection.jsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+const HeroSection = () => {
+  const images = [
+    "/assets/home/heroSection/hero1.svg",
+    "/assets/home/heroSection/hero2.svg",
+    "/assets/home/heroSection/hero3.svg",
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [nextImageIndex, setNextImageIndex] = useState(1);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  // Preload the images
+  const preloadImages = () => {
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+    });
+  };
+
+  useEffect(() => {
+    preloadImages();
+
+    const interval = setInterval(() => {
+      setIsTransitioning(true);
+
+      setTimeout(() => {
+        setCurrentImageIndex(nextImageIndex);
+        setNextImageIndex((nextImageIndex + 1) % images.length);
+        setIsTransitioning(false);
+      }, 200);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [nextImageIndex, images.length]);
+
+  return (
+    <div
+      className="relative w-full h-screen overflow-hidden"
+      style={{
+        backgroundImage: `url(${images[currentImageIndex]})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        transition: "background-image 1s ease-in-out",
+      }}
+    >
+      <div className="relative z-10 text-white px-4 sm:px-8 md:px-16 lg:px-20 h-full flex flex-col justify-center">
+        {/* Hero Title */}
+        <div className="font-ralewaySemiBold text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold max-w-2xl md:max-w-4xl leading-tight text-center md:text-left">
+          <h1>Discover Your Dream Beachfront Home</h1>
+        </div>
+
+        {/* Hero Description */}
+        <div className="font-workSansRegular text-base sm:text-lg md:text-xl lg:text-2xl opacity-80 mt-6 text-center md:text-left max-w-lg md:max-w-3xl mx-auto md:mx-0">
+          <p>
+            Experience luxury living at Camaya Coast in Mariveles Bataan
+            (Station 1, Station 2)
+          </p>
+        </div>
+
+        {/* Call-to-Action Button */}
+        <div className="mt-8 flex justify-center md:justify-start">
+          <button className="bg-[#FFC447] px-10 sm:px-14 md:px-20 py-4 sm:py-5 rounded-full font-workSansRegular text-[#221C42] text-sm sm:text-base md:text-lg">
+            View Properties
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
