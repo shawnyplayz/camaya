7902eb43429a13ac7cf227f1ce2acf0128edff59
Kaustubh <132165310+kaustubh-aras@users.noreply.github.com>
2024-12-02 10:57:02 +0530

    feat(button): allow additional classNames to be passed to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-const Button = ({ children }) => {
+const Button = ({ children, className = "" }) => {
   return (
-    <button className="bg-[#FFC447] rounded-full py-3 px-12 w-full font-workSansRegular font-normal text-xl text-[#221C42] hover:bg-[#FFB933] transition-all duration-200">
+    <button
+      className={`bg-[#FFC447] rounded-full py-3 px-12 font-workSansRegular font-normal text-xl text-[#221C42] hover:bg-[#FFB933] transition-all duration-200 ${className}`}
+    >
       {children}
     </button>
   );
