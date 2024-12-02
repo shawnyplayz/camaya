548131ea470fce2b1f5915bc21e45db3775801c7
Kaustubh <132165310+kaustubh-aras@users.noreply.github.com>
2024-12-02 12:57:25 +0530

    fix: UI bug #3

--- src/app/home/GetInTouchSection.jsx
+++ src/app/home/GetInTouchSection.jsx
@@ -6,50 +6,58 @@ import ContactForm from "@/components/ContactForm";
 const GetInTouchSection = () => {
   return (
     <div className="bg-[#FDFAF3]">
-      <div className="h-auto relative mx-auto container flex flex-col lg:flex-row items-center gap-10 lg:gap-40 px-4 lg:px-16 py-10 lg:py-16">
+      <div className="h-auto relative mx-auto container flex flex-col lg:flex-row items-center gap-8 lg:gap-20 px-4 sm:px-6 lg:px-16 py-8 sm:py-12 lg:py-16">
         {/* Left Section */}
-        <div className="left text-center lg:text-left px-4 lg:px-0">
-          <div className="pt-16 lg:pt-28 pl-4 pb-16 lg:pb-32">
-            <h1 className="font-workSansMedium font-medium text-3xl sm:text-4xl lg:text-6xl text-[#221C42]">
+        <div className="text-center lg:text-left">
+          <div className="pt-8 sm:pt-12 lg:pt-20 pl-2 pb-8 sm:pb-12 lg:pb-20">
+            <h1 className="font-workSansMedium font-medium text-2xl sm:text-3xl lg:text-5xl text-[#221C42]">
               Get in Touch
             </h1>
-            <p className="max-w-full sm:max-w-2xl mt-6 text-[#9C9C9C] font-workSansMedium font-medium text-base sm:text-lg mx-auto lg:mx-0">
+            <p className="max-w-full sm:max-w-xl mt-4 sm:mt-6 text-[#9C9C9C] font-workSansMedium font-medium text-sm sm:text-base lg:text-lg mx-auto lg:mx-0">
               {GetInTouchData.description}
             </p>
 
-            <div className="contact-info mt-10 lg:mt-20 font-workSansMedium font-medium text-lg text-[#182A64]">
-              <div className="flex items-center gap-2 mt-6 justify-center lg:justify-start">
+            <div className="contact-info mt-8 sm:mt-12 lg:mt-16 font-workSansMedium font-medium text-base sm:text-lg text-[#182A64]">
+              {/* Phone */}
+              <div className="flex items-center gap-2 mt-4 sm:mt-6 justify-center lg:justify-start">
                 <Image
                   src="assets/home/getInTouchSection/icons/phone.svg"
-                  width={24}
-                  height={24}
+                  width={20}
+                  height={20}
                   alt="Phone Icon"
                 />
-                <p>{GetInTouchData.phoneNumber}</p>
+                <p className="text-sm sm:text-base">
+                  {GetInTouchData.phoneNumber}
+                </p>
               </div>
-              <div className="flex items-center gap-2 mt-6 justify-center lg:justify-start flex-wrap">
+              {/* Email */}
+              <div className="flex items-center gap-2 mt-4 sm:mt-6 justify-center lg:justify-start">
                 <Image
                   src="assets/home/getInTouchSection/icons/mail.svg"
-                  width={26}
+                  width={20}
                   height={20}
                   alt="Email Icon"
                 />
-                <p>{GetInTouchData.email}</p>
+                <p className="text-sm sm:text-base">{GetInTouchData.email}</p>
               </div>
-              <div className="flex items-center gap-2 mt-6">
+              {/* Location */}
+              <div className="flex items-center gap-2 mt-4 sm:mt-6 justify-center lg:justify-start">
                 <Image
                   src="assets/home/getInTouchSection/icons/location.svg"
-                  width={25}
-                  height={25}
+                  width={20}
+                  height={20}
                   alt="Location Icon"
                 />
-                <p>{GetInTouchData.location}</p>
+                <p className="text-sm sm:text-base">
+                  {GetInTouchData.location}
+                </p>
               </div>
             </div>
           </div>
         </div>
 
         {/* Right Section - Contact Form */}
+        {/* <div className="mt-6 sm:mt-8 lg:mt-0 px-4 lg:px-0 w-full lg:w-auto"> */}
         <div className="mt-8 lg:mt-0 px-4 lg:px-0 2xl:absolute 2xl:right-20 lg:-bottom-44">
           <ContactForm />
         </div>
