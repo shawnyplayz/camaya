5049095af8c1abd24218acf9031bc31e218bbbf1
Kaustubh Aras <kaustubharas.dev@gmail.com>
2024-12-10 12:22:16 +0530

    Featured UI changes

--- src/app/home/FeaturedProperties.jsx
+++ src/app/home/FeaturedProperties.jsx
@@ -6,10 +6,28 @@ import { fetchDataGet } from "@/utils.js/fetchData";
 import endpoints from "@/config/endpoints";
 import defaultImage from "../../../public/assets/home/featuredPropertiesSection/image1.png";
 import oneImage from "../../../public/assets/home/featuredPropertiesSection/one_image.png";
+import twoImage from "../../../public/assets/home/featuredPropertiesSection/two_image.png";
+import threeImage from "../../../public/assets/home/featuredPropertiesSection/three_image.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
+
+import station1_1 from "../../../public/assets/home/featuredPropertiesSection/station1/1.jpg";
+import station1_2 from "../../../public/assets/home/featuredPropertiesSection/station1/2.jpg";
+import station1_3 from "../../../public/assets/home/featuredPropertiesSection/station1/3.jpg";
+import station1_4 from "../../../public/assets/home/featuredPropertiesSection/station1/4.jpg";
+import station1_5 from "../../../public/assets/home/featuredPropertiesSection/station1/5.jpg";
+
+
+import station2_1 from "../../../public/assets/home/featuredPropertiesSection/station2/1.jpg";
+import station2_2 from "../../../public/assets/home/featuredPropertiesSection/station2/2.jpg";
+import station2_3 from "../../../public/assets/home/featuredPropertiesSection/station2/3.jpg";
+import station2_4 from "../../../public/assets/home/featuredPropertiesSection/station2/4.jpg";
+
+
+import station3_1 from "../../../public/assets/home/featuredPropertiesSection/station2/1.jpg";
+import station3_2 from "../../../public/assets/home/featuredPropertiesSection/station2/2.jpg";
 
 // Custom Next and Prev arrows for the slider
 const NextArrow = ({ onClick }) => (
@@ -43,7 +61,6 @@ const FeaturedProperties = () => {
   const [propertyTypes, setPropertyTypes] = useState([]);
   const [pricingOptions, setPricingOptions] = useState([]);
   const [filteredProperties, setFilteredProperties] = useState([]);
-  const [isError, setIsError] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedFilters, setSelectedFilters] = useState({
     location: "",
@@ -67,12 +84,26 @@ const FeaturedProperties = () => {
         setFilteredProperties(defaultProperties.properties || []); // Populate initial results
       } catch (error) {
         console.error("Error fetching data:", error);
-        setIsError(true);
       }
     };
 
     fetchDropdownData();
   }, []);
+
+  const data = [
+    {
+      id: 1,
+      pictures: [{ url: station1_1 }, { url: station1_2 }],
+    },
+    {
+      id: 2,
+      pictures: [{ url: station1_1 }, { url: station1_1 }],
+    },
+    {
+      id: 3,
+      pictures: [{ url: station1_1 }, { url: station1_1 }],
+    },
+  ];
 
   const sliderSettings = {
     dots: true,
@@ -104,65 +135,176 @@ const FeaturedProperties = () => {
       </h1>
 
       <div className="mt-32">
-        {isError ? (
-          <div className="flex items-center justify-center">
-            <Image
-              src={errorImage}
-              alt="Error: No Matching Properties"
-              width={1000}
-              height={200}
-              className="pb-7"
-            />
-          </div>
-        ) : (
-          <>
-            <div className="slider-container">
-              <div className="flex gap-3">
-                <div>
-                  <Image
-                    src={oneImage}
-                    width={61}
-                    height={154}
-                    alt="one-image-feature"
-                  />
-                </div>
-                <div className="ml-[-13px]">
-                  <div className="relative top-4 left-4 flex flex-col gap-[10px]">
-                    <h2 className="font-workSansMedium font-medium text-lg sm:text-xl lg:text-2xl text-[#FFC447] uppercase">
-                      Station
-                    </h2>
-                    <h3 className="font-workSansMedium font-medium text-2xl sm:text-3xl lg:text-[3.25rem] text-[#4CAF50] uppercase italic">
-                      MARIVELES, Bataan
-                    </h3>
-                  </div>
-                </div>
+        <div className="slider-container">
+          <div className="flex gap-3">
+            <div>
+              <Image
+                src={oneImage}
+                width={61}
+                height={154}
+                alt="one-image-feature"
+              />
+            </div>
+            <div className="ml-[-13px]">
+              <div className="relative top-4 left-4 flex flex-col gap-[10px]">
+                <h2 className="font-workSansMedium font-medium text-lg sm:text-xl lg:text-2xl text-[#FFC447] uppercase">
+                  Station
+                </h2>
+                <h3 className="font-workSansMedium font-medium text-2xl sm:text-3xl lg:text-[3.25rem] text-[#4CAF50] uppercase italic">
+                  MARIVELES, Bataan
+                </h3>
               </div>
             </div>
-            <div>
-              {filteredProperties.map((property, index) => (
-                <div key={index} className="slider-container">
-                  {property?.pictures?.length > 1 ? (
-                    <Slider {...sliderSettings}>
-                      {property.pictures.map((img, i) => (
-                        <div key={i}>
-                          <Image
-                            src={img?.url || defaultImage}
-                            alt={`Property Image ${i + 1}`}
-                            width={800}
-                            height={200}
-                            className="pb-7"
-                          />
-                        </div>
-                      ))}
-                    </Slider>
-                  ) : (
-                    <></>
-                  )}
-                </div>
-              ))}
+          </div>
+        </div>
+        <div>
+          {/* {data.map((property, index) => (
+            <div key={index} className="slider-container">
+              {property?.pictures?.length > 1 ? (
+                <Slider {...sliderSettings}>
+                  {property.pictures.map((img, i) => (
+                    <div key={i}>
+                      <Image
+                        src={img?.url || defaultImage}
+                        alt={`Property Image ${i + 1}`}
+                        width={800}
+                        height={200}
+                        className="pb-7"
+                      />
+                    </div>
+                  ))}
+                </Slider>
+              ) : (
+                <></>
+              )}
             </div>
-          </>
-        )}
+          ))} */}
+          {/* {
+            data?.map((el,id)=>(
+              <div className="slider-container" key={id}>
+               
+              </div>
+            ))
+          } */}
+          
+            <div className="slider-container">
+            <Slider {...sliderSettings}>
+                  {
+                    data?.map((el,index)=>{
+                      debugger
+                      return(
+                        <div key={i}>
+                      <Image
+                        src={el?.url || defaultImage}
+                        alt={`Property Image ${i + 1}`}
+                        width={800}
+                        height={200}
+                        className="pb-7"
+                      />
+                    </div>
+                      )
+                    })
+                  }
+                </Slider>
+            </div>
+           
+          
+        </div>
+      </div>
+
+      <div className="mt-32">
+        <div className="slider-container">
+          <div className="flex gap-3">
+            <div>
+              <Image
+                src={twoImage}
+                width={80}
+                height={154}
+                alt="one-image-feature"
+              />
+            </div>
+            <div className="ml-[-13px]">
+              <div className="relative top-4 left-4 flex flex-col gap-[10px]">
+                <h2 className="font-workSansMedium font-medium text-lg sm:text-xl lg:text-2xl text-[#FFC447] uppercase">
+                  Station
+                </h2>
+                <h3 className="font-workSansMedium font-medium text-2xl sm:text-3xl lg:text-[3.25rem] text-[#4CAF50] uppercase italic">
+                  BAGAC, Bataan
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          {filteredProperties.map((property, index) => (
+            <div key={index} className="slider-container">
+              {property?.pictures?.length > 1 ? (
+                <Slider {...sliderSettings}>
+                  {property.pictures.map((img, i) => (
+                    <div key={i}>
+                      <Image
+                        src={img?.url || defaultImage}
+                        alt={`Property Image ${i + 1}`}
+                        width={800}
+                        height={200}
+                        className="pb-7"
+                      />
+                    </div>
+                  ))}
+                </Slider>
+              ) : (
+                <></>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-32">
+        <div className="slider-container">
+          <div className="flex gap-3">
+            <div>
+              <Image
+                src={threeImage}
+                width={79}
+                height={154}
+                alt="one-image-feature"
+              />
+            </div>
+            <div className="ml-[-13px]">
+              <div className="relative top-4 left-4 flex flex-col gap-[10px]">
+                <h2 className="font-workSansMedium font-medium text-lg sm:text-xl lg:text-2xl text-[#FFC447] uppercase">
+                  Station
+                </h2>
+                <h3 className="font-workSansMedium font-medium text-2xl sm:text-3xl lg:text-[3.25rem] text-[#4CAF50] uppercase italic">
+                  Balanga, Bataan
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          {filteredProperties.map((property, index) => (
+            <div key={index} className="slider-container">
+              {property?.pictures?.length > 1 ? (
+                <Slider {...sliderSettings}>
+                  {property.pictures.map((img, i) => (
+                    <div key={i}>
+                      <Image
+                        src={img?.url || defaultImage}
+                        alt={`Property Image ${i + 1}`}
+                        width={800}
+                        height={200}
+                        className="pb-7"
+                      />
+                    </div>
+                  ))}
+                </Slider>
+              ) : (
+                <></>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
