fc8546c24d85c47ce901f12ec360130a08a6707f
Kaustubh Aras <kaustubharas.dev@gmail.com>
2024-12-13 14:11:23 +0530

    Added Image

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import AOSComponent from "@/components/AOSComponent";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Head from "next/head";
+
 export const metadata = {
   title: "Camaya",
   description: "Camaya",
@@ -13,13 +14,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          property="og:image"
-          content="/assets/home/heroSection/hero6.png"
-        />
-        <meta property="og:type" content="website" />
-      </Head>
+      {/* Open Graph Meta Tags */}
+      <meta property="og:title" content="Camaya" />
+      <meta property="og:description" content="Camaya" />
+      <meta property="og:image" content="/assets/home/heroSection/hero6.png" />
+      <meta property="og:image:alt" content="About Camaya" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.camayacoastprestige.com/" />
+
+      {/* Fallback for Twitter Cards */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Camaya" />
+      <meta name="twitter:description" content="Camaya" />
+      <meta name="twitter:image" content="/assets/home/heroSection/hero6.png" />
       <body className={`antialiased`}>
         <ToastContainer
           position="top-center"
