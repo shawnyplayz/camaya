3746a5264cb3d9dd74fa7b7c7b14b68a03df2a4b
vaish567 <vaishnav.parte@gmail.com>
2025-01-06 15:17:04 +0530

    update

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -35,43 +35,14 @@ const Home = () => {
   const universalContentTextColor = searchParams.get(
     "universalContentTextColor"
   );
-  useEffect(() => {
-    // Handle dynamic styles
+
+  const applySearchParamsStyles = () => {
     if (fontName) {
-      const existingLink = document.querySelector(`link[href*="${fontName}"]`);
-      if (!existingLink) {
-        const link = document.createElement("link");
-        link.href = `https://fonts.googleapis.com/css2?family=${fontName.replace(
-          / /g,
-          "+"
-        )}&display=swap`;
-        link.rel = "stylesheet";
-        document.head.appendChild(link);
-      }
-      document.documentElement.style.setProperty(
-        "--dynamic-font-family",
-        `'${fontName}', sans-serif`
-      );
+      loadFont(fontName, "--dynamic-font-family");
     }
     if (font_name_Hero) {
-      const existingLink = document.querySelector(
-        `link[href*="${font_name_Hero}"]`
-      );
-      if (!existingLink) {
-        const link = document.createElement("link");
-        link.href = `https://fonts.googleapis.com/css2?family=${font_name_Hero.replace(
-          / /g,
-          "+"
-        )}&display=swap`;
-        link.rel = "stylesheet";
-        document.head.appendChild(link);
-      }
-      document.documentElement.style.setProperty(
-        "--hero-font-family",
-        `'${font_name_Hero}', sans-serif`
-      );
+      loadFont(font_name_Hero, "--hero-font-family");
     }
-    // Handle other color parameters
     if (navTextColor)
       document.documentElement.style.setProperty(
         "--navTextColor",
@@ -112,83 +83,92 @@ const Home = () => {
         "--universalContentTextColor",
         universalContentTextColor
       );
-  }, [
-    fontName,
-    navTextColor,
-    navIconsColor,
-    heroMainTextColor,
-    heroMainSecondaryTextColor,
-    heroSubTextColor,
-    universalButtonColor,
-    universalHeadingTextColor,
-    universalContentTextColor,
-  ]);
-  useEffect(() => {
-    const fetchServices = async () => {
-      try {
-        const response = await fetchDataGet(endpoints?.getFontColor);
-        if (Array.isArray(response) && response.length > 0) {
-          const data = response[0];
-          const fontNameFromApi = data.font_name || fontName;
+  };
 
-          // Dynamically load font from Google Fonts
-          if (fontNameFromApi) {
-            const fontUrl = `https://fonts.googleapis.com/css2?family=${fontNameFromApi.replace(
-              / /g,
-              "+"
-            )}&display=swap`;
-            if (!document.querySelector(`link[href="${fontUrl}"]`)) {
-              const link = document.createElement("link");
-              link.href = fontUrl;
-              link.rel = "stylesheet";
-              document.head.appendChild(link);
-            }
-          }
+  const fetchAndApplyStyles = async () => {
+    try {
+      const response = await fetchDataGet(endpoints?.getFontColor);
+      if (Array.isArray(response) && response.length > 0) {
+        const data = response[0];
+        const fontNameFromApi = data.font_name || fontName;
 
-          // Apply CSS variables
-          document.documentElement.style.setProperty(
-            "--dynamic-font-family",
-            `'${fontNameFromApi}', sans-serif`
-          );
-          document.documentElement.style.setProperty(
-            "--navTextColor",
-            data.navTextColor || navTextColor
-          );
-          document.documentElement.style.setProperty(
-            "--navIconsColor",
-            data.navIconsColor || navIconsColor
-          );
-          document.documentElement.style.setProperty(
-            "--heroMainTextColor",
-            data.heroMainTextColor || heroMainTextColor
-          );
-          document.documentElement.style.setProperty(
-            "--heroMainSecondaryTextColor",
-            data.heroMainSecondaryTextColor || heroMainSecondaryTextColor
-          );
-          document.documentElement.style.setProperty(
-            "--heroSubTextColor",
-            data.heroSubTextColor || heroSubTextColor
-          );
-          document.documentElement.style.setProperty(
-            "--universalButtonColor",
-            data.universalButtonColor || universalButtonColor
-          );
-          document.documentElement.style.setProperty(
-            "--universalHeadingTextColor",
-            data.universalHeadingTextColor || universalHeadingTextColor
-          );
-          document.documentElement.style.setProperty(
-            "--universalContentTextColor",
-            data.universalContentTextColor || universalContentTextColor
-          );
+        if (fontNameFromApi) {
+          loadFont(fontNameFromApi, "--dynamic-font-family");
         }
-      } catch (error) {
-        console.error("Error fetching services:", error);
-      }
-    };
 
-    fetchServices();
+        document.documentElement.style.setProperty(
+          "--navTextColor",
+          data.navTextColor || navTextColor
+        );
+        document.documentElement.style.setProperty(
+          "--navIconsColor",
+          data.navIconsColor || navIconsColor
+        );
+        document.documentElement.style.setProperty(
+          "--heroMainTextColor",
+          data.heroMainTextColor || heroMainTextColor
+        );
+        document.documentElement.style.setProperty(
+          "--heroMainSecondaryTextColor",
+          data.heroMainSecondaryTextColor || heroMainSecondaryTextColor
+        );
+        document.documentElement.style.setProperty(
+          "--heroSubTextColor",
+          data.heroSubTextColor || heroSubTextColor
+        );
+        document.documentElement.style.setProperty(
+          "--universalButtonColor",
+          data.universalButtonColor || universalButtonColor
+        );
+        document.documentElement.style.setProperty(
+          "--universalHeadingTextColor",
+          data.universalHeadingTextColor || universalHeadingTextColor
+        );
+        document.documentElement.style.setProperty(
+          "--universalContentTextColor",
+          data.universalContentTextColor || universalContentTextColor
+        );
+      }
+    } catch (error) {
+      console.error("Error fetching styles from API:", error);
+    }
+  };
+
+  const loadFont = (fontName, cssVariable) => {
+    const fontUrl = `https://fonts.googleapis.com/css2?family=${fontName.replace(
+      / /g,
+      "+"
+    )}&display=swap`;
+    if (!document.querySelector(`link[href="${fontUrl}"]`)) {
+      const link = document.createElement("link");
+      link.href = fontUrl;
+      link.rel = "stylesheet";
+      document.head.appendChild(link);
+    }
+    document.documentElement.style.setProperty(
+      cssVariable,
+      `'${fontName}', sans-serif`
+    );
+  };
+
+  useEffect(() => {
+    if (
+      fontName ||
+      navTextColor ||
+      navIconsColor ||
+      heroMainTextColor ||
+      heroMainSecondaryTextColor ||
+      heroSubTextColor ||
+      universalButtonColor ||
+      universalHeadingTextColor ||
+      universalContentTextColor
+    ) {
+      // Apply styles from searchParams if present
+      applySearchParamsStyles();
+    } else {
+      // Fetch and apply styles from API if no searchParams
+      fetchAndApplyStyles();
+    }
   }, [
     fontName,
     navTextColor,
