899f98e7326434d6bd39edf28556fe0efc3d2522
Shawn <shawn9920@gmail.com>
2025-02-06 15:15:33 +0530

    added some animations

--- src/app/home/FrequentlyAskedQuestions.jsx
+++ src/app/home/FrequentlyAskedQuestions.jsx
@@ -2,39 +2,45 @@ import Accordion from "@/components/Accordian";
 import Accordian2 from "@/components/Accordian2";
 import Accordian3 from "@/components/Accordian3";
 import StationTable from "@/components/StationTable";
-
+import { motion } from "framer-motion";
 import React from "react";
 
 const FrequentlyAskedQuestions = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-12 lg:gap-16 px-4 sm:px-5 py-10 lg:py-16">
-      {/* Header Section */}
-      <div className="left text-center">
-        <h1 className="max-w-2xl font-workSansMedium font-normal text-2xl sm:text-3xl lg:text-5xl text-universalHeadingTextColor">
-          Have Questions About Camaya Coast?
-        </h1>
-        <p className="mt-4 text-universalContentTextColor font-workSansMedium font-medium text-base sm:text-lg lg:text-xl">
-          We&rsquo;ve Got the Answers You Need!
-        </p>
-      </div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }} // Start with opacity 0 and move up
+      whileInView={{ opacity: 1, y: 0 }} // End with full opacity and normal position
+      transition={{ duration: 1, ease: "easeOut" }} // Smooth animation
+    >
+      <div className="flex flex-col items-center justify-center gap-12 lg:gap-16 px-4 sm:px-5 py-10 lg:py-16">
+        {/* Header Section */}
+        <div className="left text-center">
+          <h1 className="max-w-2xl font-workSansMedium font-normal text-2xl sm:text-3xl lg:text-5xl text-universalHeadingTextColor">
+            Have Questions About Camaya Coast?
+          </h1>
+          <p className="mt-4 text-universalContentTextColor font-workSansMedium font-medium text-base sm:text-lg lg:text-xl">
+            We&rsquo;ve Got the Answers You Need!
+          </p>
+        </div>
 
-      {/* Accordion Section */}
-      <div className="right w-full max-w-[70rem] text-universalHeadingTextColor ">
-        <Accordion
-          title="1. How to get there?"
-          content="Camaya Coast is a luxury beach resort and residential community in the Philippines."
-        />
-        <Accordian2
-          title="2. Can I set a sample computation on a best discount in the smallest lot cut?"
-          content="You can reach Camaya Coast via ferry from Manila or by land travel through Bataan."
-        />
-        <StationTable title="3. Can I get the price list?" />
-        <Accordian3
-          title="4. Is this Selling Platform headed by a licensed real estate broker or salesperson?"
-          content="Yes it is. We are headed by our Sales Director,  Ms. Maripaul Corpuz -Milanes.  She is a Licensed Real Estate Broker and Appraiser. She has been with Earth and Shore Leisure Communities Corp since 2016."
-        />
+        {/* Accordion Section */}
+        <div className="right w-full max-w-[70rem] text-universalHeadingTextColor ">
+          <Accordion
+            title="1. How to get there?"
+            content="Camaya Coast is a luxury beach resort and residential community in the Philippines."
+          />
+          <Accordian2
+            title="2. Can I set a sample computation on a best discount in the smallest lot cut?"
+            content="You can reach Camaya Coast via ferry from Manila or by land travel through Bataan."
+          />
+          <StationTable title="3. Can I get the price list?" />
+          <Accordian3
+            title="4. Is this Selling Platform headed by a licensed real estate broker or salesperson?"
+            content="Yes it is. We are headed by our Sales Director,  Ms. Maripaul Corpuz -Milanes.  She is a Licensed Real Estate Broker and Appraiser. She has been with Earth and Shore Leisure Communities Corp since 2016."
+          />
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/app/home/GetInTouchSection.jsx
+++ src/app/home/GetInTouchSection.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect } from "react";
 import { GetInTouchData } from "../constants";
 import Image from "next/image";
@@ -7,120 +8,116 @@ import { CA, PH } from "country-flag-icons/react/3x2";
 import { IoMdCall } from "react-icons/io";
 import { FaViber, FaWhatsapp } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import { AOS } from "aos";
-import "aos/dist/aos.css";
-
+import { motion } from "framer-motion";
 const GetInTouchSection = () => {
-  useEffect(() => {
-    // Initialize AOS
-    AOS?.init({
-      duration: 800,
-      easing: "ease-in-out",
-      once: false,
-    });
-  }, []);
   return (
     <section className="bg-[#FDFAF3]" id="contactUsSection">
       <div className="container mx-auto py-20 lg:py-0 lg:px-12 px-8">
         <div className="flex justify-between flex-col lg:flex-row gap-8">
-          <div
-            className="lg:relative top-[202px] leftside flex flex-col lg:text-start text-center"
-            data-aos="fade-up-right"
+          <motion.div
+            initial={{ opacity: 0, x: -100 }} // Start with opacity 0 and move up
+            whileInView={{ opacity: 1, x: 0 }} // End with full opacity and normal position
+            transition={{ duration: 1, ease: "easeOut" }} // Smooth animation
           >
-            <div className="flex flex-col gap-8">
-              <div className="flex grouped_first_half flex-col">
-                <div className="heading">
-                  <h1 className="font-workSansMedium font-medium text-2xl sm:text-3xl lg:text-5xl text-universalHeadingTextColor">
-                    Get in Touch
-                  </h1>
+            <div className="lg:relative top-[202px] leftside flex flex-col lg:text-start text-center">
+              <div className="flex flex-col gap-8">
+                <div className="flex grouped_first_half flex-col">
+                  <div className="heading">
+                    <h1 className="font-workSansMedium font-medium text-2xl sm:text-3xl lg:text-5xl text-universalHeadingTextColor">
+                      Get in Touch
+                    </h1>
+                  </div>
+                  <div className="GIT_Description">
+                    <p className="max-w-full sm:max-w-xl mt-4 sm:mt-6 text-universalContentTextColor text-center lg:text-start font-workSansMedium font-medium text-sm sm:text-base lg:text-lg mx-auto lg:mx-0">
+                      {GetInTouchData.description}
+                    </p>
+                  </div>
                 </div>
-                <div className="GIT_Description">
-                  <p className="max-w-full sm:max-w-xl mt-4 sm:mt-6 text-universalContentTextColor text-center lg:text-start font-workSansMedium font-medium text-sm sm:text-base lg:text-lg mx-auto lg:mx-0">
-                    {GetInTouchData.description}
-                  </p>
-                </div>
-              </div>
-              <div className="lg:flex lg:mr-16">
-                <div className="grid  grid-rows-[30px_auto_40px] lg:grid-cols-[30px_auto_40px] gap-x-5 gap-y-3 items-center max-w-lg mx-auto font-workSansMedium font-medium text-base text-center lg:text-start sm:text-lg text-[#182A64]">
-                  <div className="text-lg text-center flex justify-center lg:block ">
-                    {" "}
-                    <IoMdCall className="text-lg" />
-                  </div>
-                  <div className="text-universalContentTextColor text-start flex justify-center lg:block">
-                    Call - 0926 053 2470
-                  </div>
-                  <div className="hidden lg:block">
-                    <PH title="Philippines" className="w-5" />
-                  </div>
+                <div className="lg:flex lg:mr-16">
+                  <div className="grid  grid-rows-[30px_auto_40px] lg:grid-cols-[30px_auto_40px] gap-x-5 gap-y-3 items-center max-w-lg mx-auto font-workSansMedium font-medium text-base text-center lg:text-start sm:text-lg text-[#182A64]">
+                    <div className="text-lg text-center flex justify-center lg:block ">
+                      {" "}
+                      <IoMdCall className="text-lg" />
+                    </div>
+                    <div className="text-universalContentTextColor text-start flex justify-center lg:block">
+                      Call - 0926 053 2470
+                    </div>
+                    <div className="hidden lg:block">
+                      <PH title="Philippines" className="w-5" />
+                    </div>
 
-                  <div className="text-lg text-center flex justify-center lg:block">
-                    {" "}
-                    <FaViber className="text-lg" />
-                  </div>
-                  <div className="text-universalContentTextColor text-start flex justify-center lg:block">
-                    Viber - 0945 566 6565
-                  </div>
+                    <div className="text-lg text-center flex justify-center lg:block">
+                      {" "}
+                      <FaViber className="text-lg" />
+                    </div>
+                    <div className="text-universalContentTextColor text-start flex justify-center lg:block">
+                      Viber - 0945 566 6565
+                    </div>
 
-                  <div className="hidden lg:block">
-                    <PH title="Philippines" className="w-5" />
-                  </div>
-                  <div className="text-lg text-center flex justify-center lg:block">
-                    {" "}
-                    <FaWhatsapp className="text-lg" />
-                  </div>
-                  <div className="text-universalContentTextColor text-start flex justify-center lg:block">
-                    WhatsApp - 0917 635 2003
-                  </div>
-                  <div className="hidden lg:block">
-                    <PH title="Philippines" className="w-5" />
-                  </div>
+                    <div className="hidden lg:block">
+                      <PH title="Philippines" className="w-5" />
+                    </div>
+                    <div className="text-lg text-center flex justify-center lg:block">
+                      {" "}
+                      <FaWhatsapp className="text-lg" />
+                    </div>
+                    <div className="text-universalContentTextColor text-start flex justify-center lg:block">
+                      WhatsApp - 0917 635 2003
+                    </div>
+                    <div className="hidden lg:block">
+                      <PH title="Philippines" className="w-5" />
+                    </div>
 
-                  <div className="text-lg text-center flex justify-center lg:block">
-                    {" "}
-                    <IoMdCall className="text-lg" />
-                  </div>
-                  <div className="text-universalContentTextColor text-start flex justify-center lg:block">
-                    Call - 0778 241 4895
-                  </div>
-                  <div className="hidden lg:block">
-                    <CA title="Canada" className="w-5" />
-                  </div>
+                    <div className="text-lg text-center flex justify-center lg:block">
+                      {" "}
+                      <IoMdCall className="text-lg" />
+                    </div>
+                    <div className="text-universalContentTextColor text-start flex justify-center lg:block">
+                      Call - 0778 241 4895
+                    </div>
+                    <div className="hidden lg:block">
+                      <CA title="Canada" className="w-5" />
+                    </div>
 
-                  <div className="text-lg text-center flex justify-center lg:block">
-                    <MdEmail className="text-lg" />
+                    <div className="text-lg text-center flex justify-center lg:block">
+                      <MdEmail className="text-lg" />
+                    </div>
+                    <div className="text-universalContentTextColor text-start flex justify-center lg:block">
+                      <a href="mailto:lizzome.custodio@realestate.camayacoast.com">
+                        lizzome.custodio@realestate.camayacoast.com
+                      </a>
+                    </div>
+                    <div className="hidden lg:block"></div>
+                    <div className="text-lg text-center flex justify-center lg:block">
+                      <MdEmail className="text-lg" />
+                    </div>
+                    <div className="text-universalContentTextColor text-start flex justify-center lg:block">
+                      <a href="mailto:lizzume.custodio@realestate.camayacoast.com">
+                        maripaul.milanes@realestate.camayacoast.com
+                      </a>
+                    </div>
+                    <div className="hidden lg:block"></div>
+                    <div className="text-lg text-center flex justify-center lg:block">
+                      <FaLocationDot className="text-lg" />
+                    </div>
+                    <div className="text-universalContentTextColor">
+                      PET Plans Tower Guadalupe Viejo EDSA Makati City
+                    </div>
+                    <div></div>
                   </div>
-                  <div className="text-universalContentTextColor text-start flex justify-center lg:block">
-                    <a href="mailto:lizzome.custodio@realestate.camayacoast.com">
-                      lizzome.custodio@realestate.camayacoast.com
-                    </a>
-                  </div>
-                  <div className="hidden lg:block"></div>
-                  <div className="text-lg text-center flex justify-center lg:block">
-                    <MdEmail className="text-lg" />
-                  </div>
-                  <div className="text-universalContentTextColor text-start flex justify-center lg:block">
-                    <a href="mailto:lizzume.custodio@realestate.camayacoast.com">
-                      maripaul.milanes@realestate.camayacoast.com
-                    </a>
-                  </div>
-                  <div className="hidden lg:block"></div>
-                  <div className="text-lg text-center flex justify-center lg:block">
-                    <FaLocationDot className="text-lg" />
-                  </div>
-                  <div className="text-universalContentTextColor">
-                    PET Plans Tower Guadalupe Viejo EDSA Makati City
-                  </div>
-                  <div></div>
                 </div>
               </div>
             </div>
-          </div>
-          <div
-            className="rightside flex lg:relative top-[202px]"
-            data-aos="fade-up-left"
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: 100 }} // Start with opacity 0 and move up
+            whileInView={{ opacity: 1, x: 0 }} // End with full opacity and normal position
+            transition={{ duration: 1, ease: "easeOut" }} // Smooth animation
           >
-            <ContactForm />
-          </div>
+            <div className="rightside flex lg:relative top-[202px]">
+              <ContactForm />
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
